0e6a9955e60b75ac704a86319e6e1063e18a9107
Eldivan <eldivan.f1@gmail.com>
2019-11-18 19:15:58 -0300

    criacao da tabela evento no banco de dados

---
+++ src/main/java/com/evento/controller/EventoController.java
@@ -0,0 +1,28 @@
+package com.evento.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.evento.model.Evento;
+import com.evento.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento",method=RequestMethod.GET)
+	public String form() {
+		return "formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento",method=RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		return "redirect:/cadastrarEvennto";
+	}
+	
+}
---
+++ src/main/java/com/evento/model/Evento.java
@@ -0,0 +1,62 @@
+package com.evento.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import javassist.SerialVersionUID;
+
+@Entity
+@Table(name="evento")
+public class Evento implements Serializable{
+	
+	private static final long SerialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
+	
+	private String nome;
+	private String local;
+	private Calendar data;
+	private String horario;
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
+}
