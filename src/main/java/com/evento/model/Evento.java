fc44402b941ce457e42c644a9e4eb1872329c63d
Eldivan <eldivan.f1@gmail.com>
2019-11-23 07:21:30 -0300

    criando um relacionamento entre entre as entidades Evento/ Convidado

--- src/main/java/com/evento/model/Evento.java
+++ src/main/java/com/evento/model/Evento.java
@@ -1,12 +1,15 @@
 package com.evento.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -29,6 +32,9 @@ public class Evento implements Serializable{
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date data;
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> listaConvidados;
 	
 	
 	public long getCodigo() {
@@ -62,6 +68,7 @@ public class Evento implements Serializable{
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
+	
 	
 	
 }
